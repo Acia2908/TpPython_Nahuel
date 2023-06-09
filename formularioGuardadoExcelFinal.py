3ecc408623452372ae375fe3b76d55d633c6d8f9
Acia2908 <118697049+Acia2908@users.noreply.github.com>
2023-06-09 11:43:45 -0300

    segunda subida!

--- formularioGuardadoExcelFinal.py
+++ formularioGuardadoExcelFinal.py
@@ -1,122 +1,89 @@
-import pandas as pd
-from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-import openpyxl 
+import tkinter as tk
+from openpyxl import Workbook
+
+# Crear la ventana principal
+ventana = tk.Tk()
+
+#root=tk()
+#tk.title("Entrada de datos")
+#ventana.geometry("500x300")
+
+ventana.title("Formulario Ingreso Datos")
+
+def guardar_en_excel():
+    
+    datos = []
+    for row in range(3):
+        row_datos = []
+        for col in range(num_columnas.get()):
+            valor = cajas_texto[row][col].get()
+            row_datos.append(valor)
+        datos.append(row_datos)
+
+    # Crear un nuevo archivo de Excel
+    workbook = Workbook()
+    sheet = workbook.active
+
+    # Escribir los datos en las celdas a parir de la segunda
+    for row_num, row_data in enumerate(datos, start=1):
+        for col_num, value in enumerate(row_data, start=1):
+            sheet.cell(row=1, column=1, value="Apellido")# DEJE fijas las columnas de cabecera .. 
+            sheet.cell(row=2, column=1, value="Nombre")
+            sheet.cell(row=3, column=1, value="Telefono")
+            sheet.cell(row=row_num, column=col_num+1, value=value)
+
+    # Guardar el archivo .. mismo directorio ... le saque la ruta
+    workbook.save("inventarioDatosCliente.xlsx")
+    #muestro por consola como se llama el archivo que genere.. 
+    # se me pisa la informacion ... no logro que sea acumulativa ...
+    print("Datos guardados en el archivo 'inventarioDatosCliente.xlsx'.")
 
 
+# Crear el campo de entrada para el número de columnas
+num_columnas_label = tk.Label(ventana, text="Número de clientes a cargar :")
+num_columnas_label.grid(row=1, column=0)
+num_columnas = tk.IntVar()
+num_columnas_entry = tk.Entry(ventana, textvariable=num_columnas)
+num_columnas_entry.grid(row=1, column=1)
 
-root=Tk()
-root.title("Entrada de datos")
-root.geometry("500x300")
-root.title("Formulario Ingreso Datos")
+# Obtener los datos ingresados en las cajas de texto
+#seccion de Nombre
+nombre_label= tk.Label(ventana, text="Ingresa tu Apellido:")
+nombre_label.grid(row=2, column=0)
+nombre_label= tk.Label(ventana, text="Ingresa tu Nombre:")
+nombre_label.grid(row=3, column=0)
+#nombre_label.config(padx=10, pady=10)
+#seccion de Telefono
+nombre_label= tk.Label(ventana, text="Ingresa tu Telefono:")
+nombre_label.grid(row=4, column=0)
 
-miFrame=Frame(root)
-miFrame.pack()
 
-
-ListaContactos=[]
-VectorContacto=[]
-Vector=[]
-     
-
-def ventanaCapturaDatos():
-
+# Función para crear las cajas de texto
+def crear_cajas_texto():
   
-    #FRAME DE ENTRADA DE DATOS
-    
-    apellido=StringVar()
-    nombre=StringVar()
-    telefono=StringVar()
+    num_cols = num_columnas.get()
+    for row in range(3):
+        for col in range(num_cols):
+            caja_texto = tk.Entry(ventana)
+            caja_texto.grid(row=row+2, column=col+1)# le sume valores para que inicien la creacion de las cajitas luego de la primer columna de texto ... 
+            cajas_texto[row].append(caja_texto)
 
-    bienvenido = Label(miFrame, text="BIENVENIDOS")
-    bienvenido.grid(row=0, column=0, columnspan = 2)
-    bienvenido.config(font=('Arial', 16))
+ #FRAME DE ENTRADA DE DATOS
 
+bienvenido = tk.Label(ventana, text="BIENVENIDOS")
+bienvenido.grid(row=0, column=0, columnspan = 2)
+bienvenido.config(font=('Arial', 16))
 
-    #seccion de Nombre
-    nombre_label= Label(miFrame, text="Ingresa tu Nombre:")
-    nombre_label.grid(row=2, column=0)
-    nombre_label.config(padx=10, pady=10)
-    #datos de entrada nombre
-    cuadro_nombre=Entry(miFrame, justify=CENTER ,textvariable=nombre)
-    textoCaja=cuadro_nombre.get()
-    nombre.set(textoCaja)
-    cuadro_nombre.grid(row=2, column=1)
+# Crear el botón para crear las cajas de texto
+crear_button = tk.Button(ventana, text="Crear lugar de ingreso de data", command=crear_cajas_texto)
+crear_button.grid(row=3, column=3, columnspan = 2)
 
-    #-----Seccion de Apellido-----
-    apellido_label=Label(miFrame, text="Ingresa tu Apellido: ")
-    apellido_label.grid(row=3, column=0)
-    apellido_label.config(padx=10, pady=10)
-    cuadro_Apellido=Entry(miFrame, justify=CENTER ,textvariable=apellido)
-    textoCaja=cuadro_Apellido.get()
-    apellido.set(textoCaja)
-    cuadro_Apellido.grid(row=3, column=1)
+# Crear las cajas de texto iniciales
+cajas_texto = [[] for _ in range(3)]
 
+# Crear el botón para guardar los datos
+guardar_button = tk.Button(ventana, text="Guardar-Archivo", command=guardar_en_excel)
+guardar_button.grid(row=5, column=2, columnspan=2)
 
-    #Seccion de Telefono-----
-    direccion=Label(miFrame, text="Ingresa tu Telefono: ")
-    direccion.grid(row=4, column=0)
-    direccion.config(padx=10, pady=10)
-    cuadro_Telefono=Entry(miFrame, justify=CENTER ,textvariable=telefono)
-    cuadro_Telefono.grid(row=4, column=1)
-    textoCajaTel=cuadro_Telefono.get()
-    telefono.set(textoCajaTel)
-    
-
-
-
-
-    #Botones
-
-    guardamos = Button(miFrame, text="Guardar", command=guardar, bg='lightblue', fg='black')
-    guardamos.grid(row=11, column=4, columnspan = 2)
-    guardamos.config(padx=5, pady=5)
-    cuadro_Guardamos=Entry(miFrame)
-    root.mainloop()
-    cuentas= []
-    datos = {
-        "apellido": apellido.get(),
-        "Nombre": nombre.get(),
-        "telefono": telefono.get()
-    }
-    cuentas.append(datos)
-        
-    file = openpyxl.Workbook()
-    hoja= file.active
-    hoja.cell(row= 1, column= 1, value= "Nombre")
-    hoja.cell(row= 1, column= 2, value= "Apellido")
-    hoja.cell(row= 1, column= 3, value= "Telefono")
-                
-    row = 2
-    ''''
-    for valor in cuentas:
-        hoja.cell(row=row, column= 1, value=valor[0])
-        hoja.cell(row=row, column= 2, value=valor[1])
-        hoja.cell(row=row, column= 3, value=valor[2])
-        row += 1'''
-    
-               
-    file.save("C:/Users/icbc/Documents/MasterChechu/MasterChechu/cursoPython/tp1/Inventario.xlsx")
-    file.close()
-    return apellido.get(), nombre.get(), telefono.get()
-    
-    
-    
-    
-#Alerta otra ventana
-def guardar ():
-    messagebox.showinfo("Alerta", "Guardado !")
-    #Button(root, text='Close',command=root.destroy).grid(row=1, column=1, justify=CENTER )
-    
-    root.destroy()
-    
-
-
-print("Llamamos a la ventana de entrada de datos")
-
-texto=ventanaCapturaDatos()
-
-print("El texto que has introducido es:")
-print(texto)
-
+# Iniciar el bucle principal de la ventana
+ventana.mainloop()
